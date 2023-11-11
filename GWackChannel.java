77600e7e0009f88ca2267b15c05300bd9ae28979
Derek Chen <dchen36@gwmail.gwu.edu>
2023-11-11 15:29:45 -0500

    finished networking portion

---
+++ GWackChannel.java
@@ -0,0 +1,224 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.LinkedList;
+
+
+public class GWackChannel {
+    ServerSocket server;
+    LinkedList<Socket> socket_queue = new LinkedList<Socket>();
+    LinkedList<GWackConnectedClient> member_queue = new LinkedList<GWackConnectedClient>();
+
+
+    public GWackChannel(int port){
+        try{
+            server = new ServerSocket(port);
+        } catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    public void serve(){
+        while(true){
+            try{
+                //add the accepted to the socket queue
+                socket_queue.add(server.accept());
+                if(socket_queue.isEmpty()){
+                    System.out.print("error queuing");
+
+                }
+                
+                //create a new client from the socket queue
+                GWackConnectedClient new_client = new GWackConnectedClient(socket_queue.getLast());
+                addClient(new_client);
+
+                //start the thread
+                new_client.start();
+            } catch (Exception e){
+                break;
+            }
+        }
+        dequeue_all();
+    }
+
+    public void addClient(GWackConnectedClient client){
+        member_queue.add(client);
+    }
+
+    public void enqueue_msg(String msg){
+        
+    }
+
+    public void dequeue_all(){
+        //dequeue members
+        member_queue.clear();
+        
+        //dequeue sockets
+        socket_queue.clear();
+    }
+
+    //send the
+    public void getClientList(){
+        for(Socket s : socket_queue){
+            try{
+                PrintWriter pw = new PrintWriter(s.getOutputStream());
+                pw.println("START_CLIENT_LIST");
+                for(GWackConnectedClient client : member_queue){
+                    pw.println(client.getClientName());
+                }
+                pw.println("END_CLIENT_LIST");
+                pw.flush();
+            } catch (Exception e){
+                ;
+            }
+
+        }
+    }
+    
+
+    private class GWackConnectedClient extends Thread{
+        private Socket sock;
+        private String username;
+        private String msg_to_send;
+
+        public GWackConnectedClient(Socket sock){
+            this.sock = sock;
+        }
+        
+        public void sendMessage(String msg){
+            for(Socket s: socket_queue){
+                try{
+                    PrintWriter pw = new PrintWriter(s.getOutputStream());
+                    pw.println("START MSG");
+                    pw.println(msg);
+                    pw.println("END MSG");
+                    pw.flush();
+                } catch (Exception e){
+                    ;
+                }
+            }
+        }
+
+        public boolean isValid(){
+            return false;
+        }
+
+        public String getClientName(){
+            return this.username;
+        }
+
+        public void run(){
+            username = "";
+            PrintWriter pw = null;
+            BufferedReader br = null;
+
+            try{
+                pw = new PrintWriter(sock.getOutputStream());
+                br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                boolean is_connected = false;
+                boolean secret_pass = false;
+                boolean name_pass = false;
+                boolean message_pass = false;
+                while(true){
+                    //read line
+                    String message = br.readLine();
+
+                    //if there's no message, we break out
+                    if(message == null){
+                        break;
+                    }
+
+                    //if the message equals SECRET, next pass will check the key
+                    if(message.equals("SECRET")){
+                        System.out.print("SECRET READ\n");
+                        secret_pass = true;
+                        continue;
+                    }
+
+                    /*if the first line isn't the word SECRET, we can keep
+                     * looping until we get a proper start
+                     */
+
+                    /*
+                     * Secret pass: if the line is the correct key, connect the user
+                     */
+                    else if(message.equals("3c3c4ac618656ae32b7f3431e75f7b26b1a14a87")){
+                        System.out.print("SECRET PASSWORD ACCEPTED\n");
+                        is_connected = true;
+                        secret_pass = false;
+                        continue;
+                    }
+
+                    
+
+                    //name check
+                    else if(message.equals("NAME")){
+                        System.out.print("NAME READ\n");
+                        name_pass = true;
+                        continue;
+                    }
+
+                    //if the name check passed, the next line should be the username
+                    else if(name_pass == true){
+                        username = message;
+                        System.out.print("NAME RECEIVED:" + username +"\n");
+                        getClientList();
+                        name_pass = false;
+                        continue;
+                    }
+
+                    //start message check
+                    else if(message.equals("START MSG")){
+                        System.out.print("START MSG RECEIVED\n");
+                        message_pass = true;
+                        continue;
+                    }
+
+                    //end message check
+                    else if(message.equals("END MSG")){
+                        System.out.print("END MESSAGE RECEIVED\n");
+                        sendMessage(msg_to_send);
+                        msg_to_send = "";
+                        message_pass = false;
+                        continue;
+                    }
+
+                    //concantenate the string
+                    else if(message_pass == true){
+                        msg_to_send = message;
+                        continue;
+                    }
+
+                    
+                }
+
+                //close the fields
+                pw.close();
+                br.close();
+                if(socket_queue.contains(sock)){
+                    socket_queue.remove(sock);
+                    member_queue.remove(this);
+                    getClientList();
+                }
+
+                sock.close();
+            } catch (Exception e){
+                try{
+                    br.close();
+                    pw.close();
+                    sock.close();
+                }
+                //can't close the neccesary fields
+                catch (Exception f){}
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        GWackChannel server = new GWackChannel(Integer.parseInt(args[0]));
+        server.serve();
+    }
+}
--- GWackClientFrame.java
+++ GWackClientFrame.java
@@ -1,29 +1,364 @@
 import java.awt.*;
-import java.util.*;
 import javax.swing.*;
-import javax.swing.border.Border;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
 
 public class GWackClientFrame extends JFrame{
+    //panels
+    private JPanel top_panel;
+    private JPanel mid_panel;
+    private JPanel bottom_panel;
+    private JPanel mmb_panel;
+    private JPanel msg_panel;
+    private JPanel total_msg_panel;
+
+     //top panel variables
+    private JLabel name_label;
+    private JTextField name_entry;
+    private JLabel ip_label;
+    private JTextField ip_entry;
+    private JLabel port_label;
+    private JTextField port_entry;
+    private JButton disconnect_btn;
+    private JLabel members_online_label;
+    private JTextArea members_list;
+    private JScrollPane mmbers_scroll;
+    private JScrollPane msg_scroll;
+    private JScrollPane compose_scroll;
+    //message panel variables
+    private JLabel msg_label;
+    private JTextArea msg_area;
+
+    //compose variables
+    private JPanel compose_panel;
+    private JLabel compose_label;
+    private JTextArea compose_area;
+
+    //bottom panel variables
+    private JButton send;
+
+    //print writers
+    private PrintWriter pw;
+
+    //buffered readers
+
+    //sockets
+    private Socket sock;
 
     public GWackClientFrame(){
         //create the panels
-        GWackTopPanel top_panel = new GWackTopPanel(new FlowLayout());
-        GWackMidPanel mid_panel = new GWackMidPanel(new FlowLayout());
-        GWackBottomPanel bottom_panel = new GWackBottomPanel(new BorderLayout());
+        top_panel = new JPanel();
+        mid_panel = new JPanel(new FlowLayout());
+        bottom_panel = new JPanel(new BorderLayout());
+        mmb_panel = new JPanel(new BorderLayout());
+        msg_panel = new JPanel(new BorderLayout());
+        total_msg_panel = new JPanel(new BorderLayout());
+        
+        //top panel variable creation
+        name_label = new JLabel("Name: ");
+        name_entry = new JTextField(10);
+        ip_label = new JLabel("IP Address: ");
+        ip_entry = new JTextField(10);
+        port_label = new JLabel("Port: ");
+        port_entry = new JTextField(5);
+        disconnect_btn = new JButton("Connect");
+        disconnect_btn.addActionListener((e) -> {
+            if(disconnect_btn.getText().equals("Connect")){
+                connect_to_server(this);
+                disconnect_btn.setText("Disconnect");
+            }
+            else{
+                disconnect_to_server(this);
+                disconnect_btn.setText("Connect");
+            }
+        });
+
+        //scrolls
+        mmbers_scroll = new JScrollPane();
+        msg_scroll = new JScrollPane();
+        compose_scroll = new JScrollPane();
+    
+    
+
+        //add variables to top panel
+        top_panel.add(name_label);
+        top_panel.add(name_entry);
+        top_panel.add(ip_label);
+        top_panel.add(ip_entry);
+        top_panel.add(port_label);
+        top_panel.add(port_entry);
+        top_panel.add(disconnect_btn);
+
+
+        //create the variables for the message panel
+        msg_label = new JLabel("Messages");
+        msg_area = new JTextArea(10,50);
+        msg_area.setEditable(false);
+        msg_scroll.setViewportView(msg_area);
+        
+
+        //the composer
+        compose_panel = new JPanel(new BorderLayout());
+        compose_label = new JLabel("Compose");
+        compose_area = new JTextArea(3, 50);
+        compose_area.setEditable(true);
+        compose_area.addKeyListener(new KeyListener(){
+
+            public void keyTyped(KeyEvent e) {
+                if(e.getKeyChar() == '\n'){
+                    send.doClick();
+                }                
+            }
+
+            public void keyPressed(KeyEvent e) {}
+
+            public void keyReleased(KeyEvent e) {}
+
+        }); 
+        compose_scroll.setViewportView(compose_area);
+        compose_panel.add(compose_label, BorderLayout.NORTH);
+        compose_panel.add(compose_scroll, BorderLayout.SOUTH);
+        
+
+        //add to the message panel
+        msg_panel.add(msg_label, BorderLayout.NORTH);
+        msg_panel.add(msg_scroll, BorderLayout.SOUTH);
+        total_msg_panel.add(msg_panel, BorderLayout.NORTH);
+        total_msg_panel.add(compose_panel, BorderLayout.SOUTH);
+
+    
+        //creates the variables for the member panel
+        members_online_label = new JLabel("Members Online");
+        members_list = new JTextArea(15,10);
+        members_list.setEditable(false);
+        mmbers_scroll.setViewportView(members_list);
+
+        //add to the member panel
+        mmb_panel.add(members_online_label, BorderLayout.NORTH);
+        mmb_panel.add(mmbers_scroll, BorderLayout.SOUTH);
+
+        //add member and messages panel to mid panel
+        mid_panel.add(mmb_panel);
+        mid_panel.add(total_msg_panel);
+
+        //grid constraints
+        send = new JButton("Send");
+        send.addActionListener((e) -> {
+            sendMessage(this);
+        });
+        bottom_panel.add(send, BorderLayout.EAST);
+
+        
+
+        //create the frame
         this.setSize(770,200);
         this.add(top_panel, BorderLayout.NORTH);
         this.add(mid_panel, BorderLayout.CENTER);
         this.add(bottom_panel, BorderLayout.SOUTH);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
-
-
     }
+
+    public boolean connect_to_server(JFrame j){
+        //if the name, host, and port are empty
+        if(name_entry.getText().equals("")){
+            disconnect_btn.setText("Connect");
+            JOptionPane.showMessageDialog(j, "No username given");
+            return false;
+        }
+
+        if(ip_label.getText().equals("")){
+            disconnect_btn.setText("Connect");
+            JOptionPane.showMessageDialog(j, "No IP address given");
+            return false;
+        }
+
+        if(port_entry.getText().equals("")){
+            disconnect_btn.setText("Connect");
+            JOptionPane.showMessageDialog(j, "No port given");
+            return false;
+        }
+        try{
+            this.sock = new Socket(InetAddress.getByName(ip_entry.getText()), Integer.parseInt(port_entry.getText()));
+        } catch (Exception e){
+            //failed to created socket
+            JOptionPane.showMessageDialog(j, "Failed to create socket");
+            return false;
+        }
+
+        //try to pass secret and name
+        try{
+            pw = new PrintWriter(sock.getOutputStream());
+            pw.println("SECRET");
+            pw.println("3c3c4ac618656ae32b7f3431e75f7b26b1a14a87");
+            pw.println("NAME");
+            pw.println(name_entry.getText());
+            pw.flush();
+
+            //if successful, connect the ip and port and start a new thread
+            msg_area.append("CONNECTED\n");
+            GWackClientNetworking g = new GWackClientNetworking(this.sock);
+
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(j, "Login failed");
+            return false;
+        }
+
+        //login succesfull
+        return true;
+    }
+
+    public boolean disconnect_to_server(JFrame j){
+        try{
+            pw.close();
+            sock.close();
+            msg_area.setText("");
+            compose_area.setText("");
+            members_list.setText("");
+            name_entry.setText("");
+            ip_entry.setText("");
+            port_entry.setText("");
+
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(j, "Could not disconnect from server");
+        }
+        return true;
+    }
+
+    public void sendMessage(JFrame j){
+        if(sock == null || sock.isClosed()){
+            JOptionPane.showMessageDialog(j, "Connect to the server");
+            return;
+        }
+       
+        
+        compose_area.setText(compose_area.getText().replace("\n", ""));
+        try{
+            pw.println("START MSG");
+            pw.println(newMessage());
+            pw.println("END MSG");
+            pw.flush();
+            compose_area.setText("");
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(j, "Error sending message");
+        }
+    }
+
+    public String newMessage(){
+        return compose_area.getText();
+    }
+
+    private class GWackClientNetworking extends Thread{
+        private Socket sock;
+        private PrintWriter pw;
+        private BufferedReader br;
+
+
+        public GWackClientNetworking(Socket sock){
+            try{
+                this.sock = sock;
+                ReadingThread thread = new ReadingThread(sock);
+                thread.start();
+            } catch(Exception e){
+                ;
+            }
+        }
+
+        //should be synchronized so no two people write at the same time
+        public synchronized void writeMessage(String msg){
+            String handle = "[" + name_entry.getText() + "] ";
+            msg_area.append(handle + msg + "\n");
+        }
+
+        public boolean isConnected(){
+            if(sock.isClosed()){
+                return false;
+            }
+            return true;
+        }
+
+        public void disconnect(){
+            try{
+                sock.close();
+                pw.close();
+                br.close();
+            } catch (Exception e){
+                ;
+            }
+        }
+
+        private class ReadingThread extends Thread{
+            String line = "";
+            String current_list = "";
+            Socket sock;
+
+            public ReadingThread(Socket sock){
+                this.sock = sock;
+            }
+
+            public void run(){
+                boolean getting_message = false;
+                boolean getting_list = false;
+                try{
+                    BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                    if(isConnected() == false){
+                        return;
+                    }
+                    while((line = br.readLine()) != null){
+                        if(line.contains("START MSG")){
+                            getting_message = true;
+                            continue;
+                        }
+
+                        else if(line.contains("END MSG")){
+                            getting_message = false;
+                            continue;
+                        }
+
+                        else if(getting_message == true){
+                            writeMessage(line);
+                            continue;
+                        }
+
+                        else if(line.equals("START_CLIENT_LIST")){
+                            members_list.setText(null);
+                            getting_list = true;
+                            continue;
+                        }
+
+                        else if(line.equals("END_CLIENT_LIST")){
+                            String[] list_split = current_list.split("|");
+                            for(String s: list_split){
+                                s.replaceAll("|", "");
+                                members_list.append(s);
+                            }
+                            getting_list = false;
+                            current_list = "";
+                            continue;
+                        }
+                        else if(getting_list == true){
+                            current_list = current_list + line + "\n";
+                            continue;
+                        }
+                    }
+                    
+                    disconnect();
+                } catch (Exception e){
+                    ;
+                }
+            }
+        }
+
+}   
+
+    
+    
     
 }
 
